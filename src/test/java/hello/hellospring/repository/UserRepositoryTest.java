50cbd5d197c223661347f0a7ed94e2c0d821a29b
yuhyeonjo <jo.yuhyeon16@gmail.com>
2023-07-05 11:45:01 +0900

    Spring JPA : Read, Update, Delete

--- src/test/java/hello/hellospring/repository/UserRepositoryTest.java
+++ src/test/java/hello/hellospring/repository/UserRepositoryTest.java
@@ -1,12 +1,12 @@
 package hello.hellospring.repository;
 
-import hello.hellospring.HelloSpringApplication;
 import hello.hellospring.HelloSpringApplicationTests;
 import hello.hellospring.model.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public class UserRepositoryTest extends HelloSpringApplicationTests {
 
@@ -14,7 +14,7 @@ public class UserRepositoryTest extends HelloSpringApplicationTests {
     @Autowired
     private UserRepository userRepository;
 
-    @Test
+    // @Test
     public void create() {
         // String sql = insert into user(%s, %s, %d) value (account, email, age);
         User user = new User();
@@ -28,13 +28,43 @@ public class UserRepositoryTest extends HelloSpringApplicationTests {
         System.out.println("newUser : " + newUser);
     }
 
+    @Test
     public void read() {
+        Optional<User> user = userRepository.findById(2L);
 
+        user.ifPresent(selectUser -> {
+            System.out.println("user : " + selectUser);
+            System.out.println("email : " + selectUser.getEmail() );
+        });
     }
 
+    @Test
     public void update(){
+        Optional<User> user = userRepository.findById(2L);
+
+        user.ifPresent(selectUser -> {
+            selectUser.setAccount("ppp");
+            selectUser.setUpdatedAt(LocalDateTime.now());
+            selectUser.setUpdatedBy("update method()");
+
+            userRepository.save(selectUser);
+        });
 
     }
 
-    public void delete() {}
+    @Test
+    public void delete() {
+
+        Optional<User> user = userRepository.findById(2L);
+        user.ifPresent(selectUser -> {
+            userRepository.delete(selectUser);
+        });
+
+        Optional<User> deleteUser = userRepository.findById(2L);
+        if(deleteUser.isPresent()) {
+            System.out.println("데이터 존재 : " + deleteUser.get());
+        } else {
+            System.out.println("데이터 삭제 데이터 없음");
+        }
+    }
 }
